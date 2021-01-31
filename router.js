263134e78a865fcb878e797704b1726859afa6b3
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-01-31 19:13:02 +0000

    Passport JWT Enabled Part 1 - Trying to visit "/" in Postman for example , without including a valid token, gives back Unauthorized :)

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 import { signup } from "./controllers/authentication.mjs";
+import passportService from "./services/passport.mjs";
+import passport from "passport";
+
+//creating the Passport middleware
+const requireAuth = passport.authenticate("jwt", { session: false }); // when a user is authenticated, don't try to create a cookie session for them
 
 export default (app) => {
+  app.get("/", requireAuth, () => {
+    // Send them to requireAuth first, if they get through, then send them to this
+    res.send({ hi: "there" });
+  });
   app.post("/signup", signup);
 };
