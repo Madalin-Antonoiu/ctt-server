b4ed603357f19c656f59df869215bb996c36b750
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-01-31 20:50:13 +0000

    JWT Part 2: SignUp completed, with Authorization. NEXT - SIGN IN

--- router.js
+++ router.js
@@ -1,12 +1,12 @@
 import { signup } from "./controllers/authentication.mjs";
-import passportService from "./services/passport.mjs";
+import passportService from "./services/passport.mjs"; // must be included
 import passport from "passport";
 
 //creating the Passport middleware
 const requireAuth = passport.authenticate("jwt", { session: false }); // when a user is authenticated, don't try to create a cookie session for them
 
 export default (app) => {
-  app.get("/", requireAuth, () => {
+  app.get("/", requireAuth, (req, res) => {
     // Send them to requireAuth first, if they get through, then send them to this
     res.send({ hi: "there" });
   });
