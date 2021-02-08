992255f5cf7ce3527c261d3029f985e4e7c603f8
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-02-08 16:25:58 +0000

    Transformed into a class

---
+++ services/Communication.mjs
@@ -0,0 +1,115 @@
+import WebSocket from "ws";
+import { Telegraf } from "telegraf";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+class Communication {
+    constructor() {
+        this.bot = new Telegraf(process.env.BOT_TOKEN)
+        this.binanceSocket = new WebSocket(
+            `wss://stream.binance.com:9443/ws/!ticker@arr`);
+        this.globalData = [];
+        this.timeout = 10000;
+    }
+    telegramBot() {
+        this.bot.start((ctx) => ctx.reply('Welcome - /help \n - /start \n - /eth'))
+        this.bot.help((ctx) => ctx.reply('- /help \n - /start \n - /eth'))
+        this.bot.command("eth", (ctx) => ctx.reply("Wwill fetch the price , eventually..."))
+        this.bot.launch();
+        console.log("Telegraf bot launched.");
+
+    }
+    websocket() {
+        this.binanceSocket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            this.helpers.handleMessage(data);
+        };
+        this.binanceSocket.onopen = () => {
+            console.log("Stream open");
+
+            const wss = new WebSocket.Server({ port: 8080 });
+
+            wss.on("connection", function connection(ws) {
+                ws.on("message", function incoming(message) {
+                    console.log("received: %s", message);
+                });
+
+                function intervalFunc() {
+                    ws.send(JSON.stringify(globalData));
+                }
+                setInterval(intervalFunc, 1000);
+            });
+        };
+        this.binanceSocket.onclose = () => {
+            console.log("Stream closed");
+        };
+
+
+    }
+    start() {
+        this.telegramBot();
+        this.websocket();
+    }
+
+    helpers = {
+        numberWithCommas: (x) => {
+            return parseInt(x)
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
+        handleMessage: (data) => {
+            const newArray = data?.filter((obj) => {
+
+                if (obj.s.includes("USDT")) {
+
+                    // Debug
+                    // if (obj.s === "BTCUSDT") {
+                    //     console.log(obj.s + ": " + parseFloat(obj.c).toFixed(2), this.helpers.numberWithCommas(obj.v), {
+                    //         $: this.helpers.numberWithCommas(obj.q),
+                    //     });
+                    // }
+                    this.helpers.priceAlert(obj, "BTCUSDT", 43650);
+
+
+
+                    return obj;
+                }
+
+
+            });
+            this.globalData = newArray;
+        },
+        priceAlert: (obj, symbol, targetPrice) => {
+            let incomingSymbol = obj.s;
+
+            if (incomingSymbol === symbol) {
+                let zeroPointZeroFivePercent = (0.0005 * targetPrice); // If price is 1650$, this is 4.125$;
+                let closedPrice = obj.c;
+
+                if (closedPrice === targetPrice) {
+                    let reply = `${symbol} is ${targetPrice}.`
+                    console.log(reply);
+
+                    this.bot.telegram.sendMessage("-587747842", reply)
+                } else if (closedPrice > targetPrice && closedPrice <= targetPrice + zeroPointZeroFivePercent
+                ) {
+                    let reply = `${symbol} is ${parseFloat(closedPrice - targetPrice).toFixed(2)} more than target price of ${targetPrice}.`
+                    this.bot.telegram.sendMessage("-587747842", reply)
+                    console.log(reply);
+                } else if (closedPrice < targetPrice && closedPrice >= targetPrice - zeroPointZeroFivePercent) {
+                    let reply = `${symbol} is ${parseFloat(targetPrice - closedPrice).toFixed(2)} less than target price of ${targetPrice}.`
+                    console.log(reply);
+                    this.bot.telegram.sendMessage("-587747842", reply)
+                }
+            }
+        }
+    }
+
+}
+
+
+
+
+
+export default Communication;
