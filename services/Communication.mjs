2e9249ea558434020a1470272d795e01542d7db9
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-02-10 22:59:22 +0000

    Things lSavingook alright. DONE 0m-60m for a set coin

--- services/Communication.mjs
+++ services/Communication.mjs
@@ -18,7 +18,10 @@ class Communication {
             `wss://stream.binance.com:9443/ws/!ticker@arr`);
         this.bot = new Telegraf(process.env.BOT_TOKEN)
         this.coinsUSDT = [];
+        this.allCoinsTracked = "";
         this.timeout = 10000;
+
+
 
         //redis
         this.redisOptions = {
@@ -61,7 +64,7 @@ class Communication {
                 });
 
                 function intervalFunc() {
-                    ws.send(JSON.stringify(coinsUSDT));
+                    ws.send(JSON.stringify(this.coinsUSDT));
                 }
                 setInterval(intervalFunc, 1000);
             });
@@ -157,6 +160,24 @@ class Communication {
         };
         this.binanceSocket.onopen = () => {
             console.log("Stream open");
+
+            const wss = new WebSocket.Server({ port: 8080 });
+
+            const callback = (ws) => {
+                ws.send(JSON.stringify(this.allCoinsTracked));
+            }
+
+            wss.on("connection", function connection(ws) {
+                ws.on("message", function incoming(message) {
+                    console.log("received: %s", message);
+                });
+
+                setInterval(() => callback(ws), 1000)
+            });
+
+
+
+
         };
         this.binanceSocket.onclose = () => {
             console.log("Stream closed");
@@ -186,7 +207,7 @@ class Communication {
             // In reality, i should read the value first, extract last and leave it there, for 1h data
             // if length is at least 59, then remove stuff, with cron job
             this.redisClient.set("usdtMarketPrices", "");
-            this.bot.telegram.sendMessage(this.golemDebugID, "Debug, every hour i  clear Redis 'usdtMarketPrices' ");
+            this.bot.telegram.sendMessage(this.golemDebugID, "######### Debug, every hour i  clear Redis 'usdtMarketPrices' ###########");
         }, 3600000) //1h
 
 
@@ -296,75 +317,50 @@ class Communication {
                 if (reply !== "") {
                     let redisAsObj = JSON.parse(reply);
 
-                    const combineForMinutesAgo = (redisObj, coinsUSDT, minute) => {
-                        let ago = Number(minute) + 1;
-                        const time = redisObj[redisObj.length - ago];
+                    let _0, _1, _3, _5, _10, _15, _30, _60 = "";
 
-                        if (time === undefined) return
-
-                        let arr1 = time.USDT_ALL;
-                        let arr2 = coinsUSDT.data;
-                        const merged = this.helpers.merge(arr1, arr2)
-                        return { wsTime: coinsUSDT.time, time: time.serverTime, data: merged, minute: minute + "m" }; // this is 1s ago to 59 s ago, then renews 
-
-                    }
-                    const trackCoin = (name, parentObject) => {
-                        if (parentObject === undefined) return
-
-                        const coin = name;
-                        const symbol = parentObject.data?.find(obj => obj.symbol === coin);
-
-                        let priceNow = symbol.c; //number
-                        let redisPrice = symbol.price;
-                        let percentageDiff = null;
-
-                        if (priceNow >= redisPrice) {
-                            percentageDiff = "+" + this.helpers.percentageDiff(Number(priceNow), Number(redisPrice)) + "%";
-                        }
-                        if (redisPrice > priceNow) {
-                            percentageDiff = "-" + this.helpers.percentageDiff(Number(redisPrice), Number(priceNow)) + "%";
-                        }
-
-                        //console.log(`ETHUSDT:`, parseFloat(priceNow), " vs ", parseFloat(obj.price), `(${pastMinute.time.replace("09/02/2021,", "")} )`)
-                        return {
-                            percentageDiff: `${coin}: (${parentObject.minute}) ${percentageDiff}`,
-                            data: `${coin} (${parentObject.minute}): ${priceNow}(${percentageDiff}) vs  ${redisPrice}(${parentObject.time.split(", ").slice(1)})`
-                        }
-
-                    }
-
-                    // combined data per past minute
-
-                    const _0m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "0")
-                    const _1m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "1")
-                    const _3m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "3")
-                    // const _5m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "6") 
-                    // const _10m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "11") 
-                    // const _15m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "16") 
-                    // const _30m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "31") 
-                    // const _60m = combineForMinutesAgo(redisAsObj, this.coinsUSDT, "61") 
+                    const _0m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "0")
+                    const _1m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "1")
+                    const _3m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "3")
+                    const _5m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "5")
+                    const _10m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "10")
+                    const _15m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "15")
+                    const _30m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "30")
+                    const _60m = this.helpers.combineForMinutesAgo(redisAsObj, this.coinsUSDT, "60")
 
 
 
-                    if (_0m !== undefined) {
-                        console.log(trackCoin("DOGEUSDT", _0m).data);
-                    }
+                    // trackCoinEveryInterval("PNT");
 
-                    if (_1m !== undefined) {
-                        console.log(trackCoin("DOGEUSDT", _1m).data);
-                    }
-                    if (_3m !== undefined) {
-                        console.log(trackCoin("DOGEUSDT", _3m).data);
-                    }
+                    // let pair = "PNT" + "USDT";
+
+                    // _0m ? _0 = this.helpers.trackCoin(pair, _0m).percentageDiff : "";
+                    // _1m ? _1 = this.helpers.trackCoin(pair, _1m).percentageDiff : "";
+                    // _3m ? _3 = this.helpers.trackCoin(pair, _3m).percentageDiff : "";
+                    // _5m ? _5 = this.helpers.trackCoin(pair, _5m).percentageDiff : "";
+                    // _10m ? _10 = this.helpers.trackCoin(pair, _10m).percentageDiff : "";
+                    // _15m ? _15 = this.helpers.trackCoin(pair, _15m).percentageDiff : "";
+                    // _30m ? _30 = this.helpers.trackCoin(pair, _30m).percentageDiff : "";
+                    // _60m ? _60 = this.helpers.trackCoin(pair, _60m).percentageDiff : "";
+                    // console.log(`${pair} :`, _0, _1, _3, _5, _10, _15, _30, _60);
+
+
+                    // console.log(this.helpers.trackAllCoins(_0m));
+                    const response = this.helpers.trackAllCoins(_0m);
+                    // console.log(response);
+
+                    this.allCoinsTracked = response;
+                    // console.log(this.allCoinsTracked);
+                    //create a combined object containing all differences
+
+                    // if ws open with client, send it
+
+
 
                 }
 
             })
 
-            // Compare price now with prices there
-            // Say something about it 
-
-            //console.log(this.coinsUSDT); // Around 80.500 length containing all USDT coins data, updated every second
         },
         savePricesToRedisEveryMinute: async () => {
             const response = await getUSDTPrices();
@@ -412,8 +408,78 @@ class Communication {
 
             return (100 * Math.abs((a - b) / ((a + b) / 2))).toFixed(2);
 
-        }
+        },
 
+        //Related
+        combineForMinutesAgo: (redisObj, coinsUSDT, minute) => {
+            let ago = Number(minute) + 1;
+            const time = redisObj[redisObj.length - ago];
+
+            if (time === undefined) return
+
+            let arr1 = time.USDT_ALL;
+            let arr2 = coinsUSDT.data;
+            const merged = this.helpers.merge(arr1, arr2)
+            return { wsTime: coinsUSDT.time, time: time.serverTime, data: merged, minute: minute + "m" }; // this is 1s ago to 59 s ago, then renews 
+
+        },
+        trackCoin: (name, parentObject) => {
+            if (parentObject === undefined) return
+
+            const coin = name;
+            const symbol = parentObject.data?.find(obj => obj.symbol === coin);
+
+            let storageTime = parentObject.time.split(", ").slice(1);
+            let priceNow = symbol.c;
+            let redisPrice = symbol.price;
+            let percentageDiff = null;
+
+            if (priceNow >= redisPrice) {
+                percentageDiff = "+" + this.helpers.percentageDiff(Number(priceNow), Number(redisPrice)) + "%";
+            }
+            if (redisPrice > priceNow) {
+                percentageDiff = "-" + this.helpers.percentageDiff(Number(redisPrice), Number(priceNow)) + "%";
+            }
+
+            //console.log(`ETHUSDT:`, parseFloat(priceNow), " vs ", parseFloat(obj.price), `(${pastMinute.time.replace("09/02/2021,", "")} )`)
+            return {
+                percentageDiff: `${percentageDiff} (${parentObject.minute} [${storageTime}])`,
+                data: `${coin} (${parentObject.minute}): ${priceNow}(${percentageDiff}) vs  ${redisPrice}(${storageTime})`
+            }
+
+        },
+        trackAllCoins: (parentObject) => { //track all coins 0m, 1m, 3m..
+            if (parentObject === undefined) return
+
+            let storageTime = parentObject.time.split(", ").slice(1);
+
+            const symbol = parentObject.data?.map(obj => {
+                // For every of the 259 coins...
+
+                let priceNow = obj.c;
+                let redisPrice = obj.price;
+                let percentageDiff = null;
+
+                if (priceNow >= redisPrice) {
+                    percentageDiff = "+" + this.helpers.percentageDiff(Number(priceNow), Number(redisPrice)) + "%";
+                }
+                if (redisPrice > priceNow) {
+                    percentageDiff = "-" + this.helpers.percentageDiff(Number(redisPrice), Number(priceNow)) + "%";
+                }
+
+                //console.log(`ETHUSDT:`, parseFloat(priceNow), " vs ", parseFloat(obj.price), `(${pastMinute.time.replace("09/02/2021,", "")} )`)
+                return {
+                    coin: obj.symbol,
+                    comparedTo: parentObject.minute,
+                    priceNow: priceNow,
+                    percentageDiff: percentageDiff,
+                    priceBackThen: redisPrice,
+                    timeBackThen: storageTime[0]
+                }
+            });
+
+            return symbol;
+        }
     }
 }
 
